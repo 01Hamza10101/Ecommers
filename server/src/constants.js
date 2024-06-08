0b9a4bcc577c7b4f8b22cf8dff7eda4b791f930e
01Hamza10101 <hamzah0786029@gmail.com>
2024-06-08 21:50:34 +0530

    db connect

---
+++ server/src/constants.js
@@ -0,0 +1,2 @@
+export const DB_NAME = "test";
+export const MONGODB_URI = "mongodb+srv://new_userEcommer1234:new_userEcommer1234@cluster0.kblhpgg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
